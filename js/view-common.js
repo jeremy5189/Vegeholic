5f676094c185bf41c54d178a0c28f7b171c76452
Jeremy Yen <jeremy5189@gmail.com>
2016-08-09 18:57:10 +0200

    Prepare to work on recipe page content

--- js/view-common.js
+++ js/view-common.js
@@ -2,3 +2,32 @@ $(window).load(function() {
 	// Animate loader off screen
 	$(".loading-icon").fadeOut("slow");;
 });
+
+var navbar = new Vue({
+	el: '#navbar-wrap',
+	data: {
+		system_name: '全植食尚',
+		buttons: [
+			{
+				title: '回首頁',
+				links: 'index.html'
+			},
+			{
+				title: '找食譜',
+				links: 'recipe.html'
+			},
+			{
+				title: '讀食記',
+				links: '#blog.html'
+			},
+			{
+				title: '逛市集',
+				links: '#market.html'
+			},
+			{
+				title: '看影片',
+				links: '#video.html'
+			}
+		]
+	}
+});
--- js/view-recipe.js
+++ js/view-recipe.js
@@ -6,25 +6,25 @@ var v_recipe_list = new Vue({
             image_path: 'img/recipe/1.png',
             author_avatar: 'https://pbs.twimg.com/profile_images/650405543782121473/XyWJo5Xn.jpg',
             author_name: 'Donald J. Trump',
-            link: '#'
+            link: 'recipe-example.html?id=1'
         }, {
             title: 'Special Interest Fruit',
             image_path: 'img/recipe/2.png',
             author_avatar: 'https://static-s.aa-cdn.net/img/ios/1084057530/54ff19e976e0eda8b92fca8d42a3e142?v=1',
             author_name: 'Hillary Clinton',
-            link: '#'
+            link: 'recipe-example.html?id=2'
         }, {
             title: 'Exile Salad',
             image_path: 'img/recipe/3.png',
             author_avatar: 'https://pbs.twimg.com/profile_images/730873252554412032/5-w-A3rn.jpg',
             author_name: 'Fethullah Gülen',
-            link: '#'
+            link: 'recipe-example.html?id=3'
         }, {
             title: 'Ottoman Empire Drink',
             image_path: 'img/recipe/4.png',
             author_avatar: 'http://media.nu.nl/m/bttxhhza2kig_sqr256.jpg/erdogan-wil-leger-en-inlichtingendienst-turkije-aanvoeren.jpg',
             author_name: 'Recep Tayyip Erdoğan',
-            link: '#'
+            link: 'recipe-example.html?id=4'
         }, ]
     }
 });
