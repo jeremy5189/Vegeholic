5f5497a2d9eecfbf29912d14634b5fed7097626f
Jeremy Yen <jeremy5189@gmail.com>
2016-08-10 18:27:48 +0200

    Fix template

--- js/view-recipe-page.js
+++ js/view-recipe-page.js
@@ -1,6 +1,45 @@
 new Vue({
-    el: '#recipe-step',
+    el: '#recipe-panel',
     data: {
+        info: {
+            name: '蘇維埃香菇雞肉炒麵',
+            img_link: 'img/slide-1.jpg',
+            author: {
+                name: '蘇維埃共產黨',
+                recipe_count: 204,
+                fan_count: 1888,
+                avatar: 'img/placeholder/us.png'
+            },
+            stat: {
+                view: 2705,
+                like: 90,
+                reply: 5,
+                date: '2016.08.02'
+            }
+        },
+        text: '1950 年代，匈牙利的三明治和蘇聯的三明治有什麼不同呢？匈牙利的三明治是：麵包、麵包、中間再夾一片麵包。蘇聯的三明治是：麵包兌換券、麵包兌換券、中間再夾一張火腿兌換券',
+        ingredients: [
+            {
+                item: '香菇兌換卷',
+                amount: '3張'
+            },
+            {
+                item: '雞肉兌換卷',
+                amount: '2張'
+            },
+            {
+                item: '乾麵團兌換卷',
+                amount: '1張'
+            },
+            {
+                item: '<del>美帝主義</del>檸檬兌換卷<span class="label label-danger">政治不正確警告</span>',
+                amount: '1張'
+            },
+            {
+                item: '馬克思共產調味料',
+                amount: '5g'
+            }
+        ],
         steps: [
             {
                 img: 'img/new/1.jpg',
--- js/view-recipe.js
+++ js/view-recipe.js
@@ -4,25 +4,25 @@ var v_recipe_list = new Vue({
         recipe_list: [{
             title: 'Mexican Drink',
             image_path: 'img/recipe/1.png',
-            author_avatar: 'https://pbs.twimg.com/profile_images/650405543782121473/XyWJo5Xn.jpg',
+            author_avatar: 'img/placeholder/us.png',
             author_name: 'Donald J. Trump',
             link: 'recipe-example.html?id=1'
         }, {
             title: 'Special Interest Fruit',
             image_path: 'img/recipe/2.png',
-            author_avatar: 'https://static-s.aa-cdn.net/img/ios/1084057530/54ff19e976e0eda8b92fca8d42a3e142?v=1',
+            author_avatar: 'img/placeholder/uk.png',
             author_name: 'Hillary Clinton',
             link: 'recipe-example.html?id=2'
         }, {
             title: 'Exile Salad',
             image_path: 'img/recipe/3.png',
-            author_avatar: 'https://pbs.twimg.com/profile_images/730873252554412032/5-w-A3rn.jpg',
+            author_avatar: 'img/placeholder/ur.png',
             author_name: 'Fethullah Gülen',
             link: 'recipe-example.html?id=3'
         }, {
             title: 'Ottoman Empire Drink',
             image_path: 'img/recipe/4.png',
-            author_avatar: 'http://media.nu.nl/m/bttxhhza2kig_sqr256.jpg/erdogan-wil-leger-en-inlichtingendienst-turkije-aanvoeren.jpg',
+            author_avatar: 'img/placeholder/ir.png',
             author_name: 'Recep Tayyip Erdoğan',
             link: 'recipe-example.html?id=4'
         }, ]
