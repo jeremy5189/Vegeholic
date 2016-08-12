7152e205e3e3cbfb1630b7e49695eb398aee6a33
Jeremy Yen <jeremy5189@gmail.com>
2016-08-12 17:33:29 +0200

    Change index navbar style

--- js/view-common.js
+++ js/view-common.js
@@ -2,30 +2,46 @@ $(window).load(function() {
 	// Animate loader off screen
 	$(".loading-icon").fadeOut("slow");;
 });
+$(function(){
+	$('#sticky_navbar').hide();
+});
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    // Do something
+	//console.log(scroll);
+	if( scroll < 130 ) {
+		$('#sticky_navbar').hide();
+		//$('#great_navbar').show();
+	} else {
+		// 顯示 sticky_navbar
+		$('#sticky_navbar').show();
+		//$('#great_navbar').hide();
+	}
+});
 
 var navbar = new Vue({
-	el: '#navbar-wrap',
+	el: '.navbar-wrap',
 	data: {
 		system_name: '全植食尚',
 		buttons: [
 			{
-				title: '回首頁',
+				title: '回首頁 Homepage',
 				links: 'index.html'
 			},
 			{
-				title: '找食譜',
+				title: '找食譜 Recipe',
 				links: 'recipe.html'
 			},
 			{
-				title: '讀食記',
+				title: '讀食記 Blog',
 				links: '#blog.html'
 			},
 			{
-				title: '逛市集',
+				title: '逛市集 Market',
 				links: '#market.html'
 			},
 			{
-				title: '看影片',
+				title: '看影片 Videos',
 				links: '#video.html'
 			}
 		]
